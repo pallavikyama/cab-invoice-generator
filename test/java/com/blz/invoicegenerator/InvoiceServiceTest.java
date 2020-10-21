5ebfad9408ae3a2b84d41b0977c5ca5c7c2f4095
pallavikyama <kyamap98@gmail.com>
2020-10-21 12:49:39 +0530

    Added:Took multiple rides and calculated aggregate total cost.

--- test/java/com/blz/invoicegenerator/InvoiceServiceTest.java
+++ test/java/com/blz/invoicegenerator/InvoiceServiceTest.java
@@ -23,4 +23,12 @@ public class InvoiceServiceTest {
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		assertEquals(5, fare, 0.0);
 	}
+
+	@Test
+	public void givenMultipleRides_ShouldReturnAggregateFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		double fare = invoiceGenerator.calculateFare(rides);
+		assertEquals(30, fare, 0.0);
+	}
 }
