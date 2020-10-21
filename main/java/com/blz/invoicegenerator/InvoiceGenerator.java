ea1231715090141ba8cc864b47d3e485ff8c56df
pallavikyama <kyamap98@gmail.com>
2020-10-21 12:02:15 +0530

    Added:Total fare is calculated based on distance and time of travel. Minimum fare is allotted for lesser fare. Related test cases are also validated.

--- main/java/com/blz/invoicegenerator/InvoiceGenerator.java
+++ main/java/com/blz/invoicegenerator/InvoiceGenerator.java
@@ -1,7 +1,15 @@
 package com.blz.invoicegenerator;
 
 public class InvoiceGenerator {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Cab Invoice Generator Services.");
+
+	private static final double COST_PER_KM = 10.0;
+	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5.0;
+
+	public double calculateFare(double distance, int time) {
+		double totalFare = distance * COST_PER_KM + time * COST_PER_TIME;
+		if (totalFare < MINIMUM_FARE)
+			totalFare = MINIMUM_FARE;
+		return totalFare;
 	}
 }
